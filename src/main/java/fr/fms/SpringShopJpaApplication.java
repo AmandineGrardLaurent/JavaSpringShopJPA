4b8c692da94d5b8a86af414ef95f558ee926ea8f
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-03-24 14:02:17 +0100

    Modification de l'application pour utiliser les services

--- src/main/java/fr/fms/SpringShopJpaApplication.java
+++ src/main/java/fr/fms/SpringShopJpaApplication.java
@@ -4,30 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 
-import fr.fms.dao.CategoryRepository;
-import fr.fms.dao.ArticleRepository;
 import fr.fms.entities.Category;
+import fr.fms.service.ArticleService;
+import fr.fms.service.CategoryService;
 import fr.fms.entities.Article;
-
-import java.util.Optional;
 
 
 @SpringBootApplication
 public class SpringShopJpaApplication implements CommandLineRunner{
 	@Autowired
-	private CategoryRepository categoryRepository;
+	private CategoryService categoryService;
 
 	@Autowired
-	private ArticleRepository articleRepository;
+	private ArticleService articleService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringShopJpaApplication.class, args);
 	}
 
 	@Override
-	@Transactional
 	public void run(String... args) throws Exception {
 		// Category smartphone = categoryRepository.save(new Category("Smartphone"));
 		// Category tablet = categoryRepository.save(new Category("Tablet"));
@@ -41,63 +37,45 @@ public class SpringShopJpaApplication implements CommandLineRunner{
 		// articleRepository.save(new Article(null,"Ipad", "Apple", 350, tablet));
 
 		System.out.println("Affichage des articles de la catégorie 1 : ");
-		for(Article article : articleRepository.findByCategoryId(1L)){
+		for(Article article : articleService.getArticleByCategoryId(1L)){
 			System.out.println(article);
 		}
 
 		System.out.println("Affiche d'un article 1 : ");
-		System.out.println(articleRepository.findById(1L));
+		System.out.println(articleService.getArticleById(1L));
 
 		System.out.println("Affichage de tous les articles : ");
-		for(Article article : articleRepository.findAll()){
+		for(Article article : articleService.getAllArticles()){
 			System.out.println(article);
 		}
 
 		System.out.println("Affichage de tous les articles de la marque Samsung et serie S: ");
-		for(Article article : articleRepository.searchArticles("Sam", "S")){
+		for(Article article : articleService.getArticleByBrandContainingAndDescriptionContaining("Sam", "S")){
 			System.out.println(article);
 		}
 
 		// System.out.println("Suppression de l'id 3");
-		// articleRepository.deleteById(3L);
-
-		System.out.println("Affichage de tous les articles : ");
-		for(Article article : articleRepository.findAll()){
-			System.out.println(article);
-		}
+		// articleService.deleteArticle(3L);
 
 		System.out.println("Modification du prix de l'article 1  : 350 -> 300");
-		Optional<Article> optional = articleRepository.findById(1L);
-		if(optional.isPresent()){
-			Article article = optional.get();
-			article.setPrice(300);
-			articleRepository.save(article);
-		}
+		articleService.updatePrice(1L, 300);
 
 		System.out.println("Modification de la description de l'article 1  : S10 -> S11");
-		Optional<Article> optional1 = articleRepository.findById(1L);
-		if(optional.isPresent()){
-			Article article = optional1.get();
-			article.setDescription("S11");
-			articleRepository.save(article);
-		}
-
-		System.out.println("Affiche d'un article 2 : ");
-		System.out.println(articleRepository.findById(1L));
+		articleService.updateDescription(1L, "S11");
 
 
 		System.out.println("Affichage de toutes les catégories par ordre croissant : ");
-		for(Category category : categoryRepository.findAllByOrderByNameAsc()){
+		for(Category category : categoryService.getAllCategoryByOrderByNameAsc()){
 			System.out.println(category);
 		}
 
 		System.out.println("Affichage de toutes les catégories par ordre décroissant : ");
-		for(Category category : categoryRepository.findAllByOrderByNameDesc()){
+		for(Category category : categoryService.getAllCategoryByOrderByNameDesc()){
 			System.out.println(category);
 		}
 
 		// System.out.println("Suppression des articles moins de 301€");
-		// articleRepository.deleteByPriceLessThan(301);
+		// articleService.deleteArticleByPriceLessThan(301);
 
 	}
 }
