5081caee7c378bfebe29efe3ef9d18633f73ef29
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-03-24 15:04:06 +0100

    Ajout de  la méthode displayItems dans la classe Helper + class consoleColors

--- src/main/java/fr/fms/utils/Helper.java
+++ src/main/java/fr/fms/utils/Helper.java
@@ -1,15 +1,24 @@
 package fr.fms.utils;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Helper {
+
+    public class ConsoleColors {
+        public static final String RESET = "\u001B[0m";
+        public static final String RED = "\u001B[31m";
+        public static final String GREEN = "\u001B[32m";
+        public static final String YELLOW = "\u001B[33m";
+        public static final String BLUE = "\u001B[34m";
+    }
 
     public static int askInt(Scanner scanner, String message) {
         int number;
         while (true) {
             System.out.println(message);
             if (!scanner.hasNextInt()) {
-                System.out.println("Saisissez un entier.");
+                System.out.println(ConsoleColors.RED + "Saisissez un entier." + ConsoleColors.RESET);
                 scanner.next();
                 continue;
             }
@@ -37,6 +46,18 @@ public class Helper {
                 + "\n[12] Quitter\n");
 
         // Ask the user for a numeric choice
-        return askInt(scanner, "Quel est votre choix ? ");
+        return askInt(scanner, "Quel est votre choix ? " + ConsoleColors.RESET);
+    }
+
+    public static <T> void displayItems(List<T> items, String title, String errorMessage) {
+        if (items.isEmpty()) {
+            System.out.println(ConsoleColors.RED + errorMessage + ConsoleColors.RESET);
+        } else {
+            System.out.println(ConsoleColors.BLUE + title + ConsoleColors.RESET + "\n");
+            for (T item : items) {
+                System.out.println(item);
+            }
+            System.out.println("\n");
+        }
     }
 }
