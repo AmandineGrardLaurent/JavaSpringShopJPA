86604541c6ee79d6b7d9595b12976f8fa48adc3f
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-03-24 14:45:31 +0100

    Affichage des choix possibles + exit de l'application

--- src/main/java/fr/fms/SpringShopJpaApplication.java
+++ src/main/java/fr/fms/SpringShopJpaApplication.java
@@ -3,16 +3,19 @@ package fr.fms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
+
+import java.util.Scanner;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import fr.fms.entities.Category;
 import fr.fms.service.ArticleService;
 import fr.fms.service.CategoryService;
+import fr.fms.utils.Helper;
 import fr.fms.entities.Article;
 
-
 @SpringBootApplication
-public class SpringShopJpaApplication implements CommandLineRunner{
+public class SpringShopJpaApplication implements CommandLineRunner {
 	@Autowired
 	private CategoryService categoryService;
 
@@ -36,47 +39,90 @@ public class SpringShopJpaApplication implements CommandLineRunner{
 
 		// articleRepository.save(new Article(null,"Ipad", "Apple", 350, tablet));
 
-		System.out.println("Affichage des articles de la catégorie 1 : ");
-		for(Article article : articleService.getArticleByCategoryId(1L)){
-			System.out.println(article);
+		Scanner scanner = new Scanner(System.in);
+		boolean exit = false;
+
+		System.out.println("Bienvenue dans notre application de gestion d'articles");
+
+		while (!exit) {
+			int userChoice = Helper.displayChoices(scanner);
+			switch (userChoice) {
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+				case 6:
+					break;
+				case 7:
+					break;
+				case 8:
+					break;
+				case 9:
+					break;
+				case 10:
+					break;
+				case 11:
+					break;
+				case 12:
+					System.out.println("Bonne journée et à bientôt");
+					exit = true;
+					break;
+				default:
+					break;
+			}
 		}
 
-		System.out.println("Affiche d'un article 1 : ");
-		System.out.println(articleService.getArticleById(1L));
+		// System.out.println("Affichage des articles de la catégorie 1 : ");
+		// for(Article article : articleService.getArticleByCategoryId(1L)){
+		// System.out.println(article);
+		// }
 
-		System.out.println("Affichage de tous les articles : ");
-		for(Article article : articleService.getAllArticles()){
-			System.out.println(article);
-		}
+		// System.out.println("Affiche d'un article 1 : ");
+		// System.out.println(articleService.getArticleById(1L));
 
-		System.out.println("Affichage de tous les articles de la marque Samsung et serie S: ");
-		for(Article article : articleService.getArticleByBrandContainingAndDescriptionContaining("Sam", "S")){
-			System.out.println(article);
-		}
+		// System.out.println("Affichage de tous les articles : ");
+		// for(Article article : articleService.getAllArticles()){
+		// System.out.println(article);
+		// }
 
-		// System.out.println("Suppression de l'id 3");
-		// articleService.deleteArticle(3L);
+		// System.out.println("Affichage de tous les articles de la marque Samsung et
+		// serie S: ");
+		// for(Article article :
+		// articleService.getArticleByBrandContainingAndDescriptionContaining("Sam",
+		// "S")){
+		// System.out.println(article);
+		// }
 
-		System.out.println("Modification du prix de l'article 1  : 350 -> 300");
-		articleService.updatePrice(1L, 300);
+		// // System.out.println("Suppression de l'id 3");
+		// // articleService.deleteArticle(3L);
 
-		System.out.println("Modification de la description de l'article 1  : S10 -> S11");
-		articleService.updateDescription(1L, "S11");
+		// System.out.println("Modification du prix de l'article 1 : 350 -> 300");
+		// articleService.updatePrice(1L, 300);
 
+		// System.out.println("Modification de la description de l'article 1 : S10 ->
+		// S11");
+		// articleService.updateDescription(1L, "S11");
 
-		System.out.println("Affichage de toutes les catégories par ordre croissant : ");
-		for(Category category : categoryService.getAllCategoryByOrderByNameAsc()){
-			System.out.println(category);
-		}
+		// System.out.println("Affichage de toutes les catégories par ordre croissant :
+		// ");
+		// for(Category category : categoryService.getAllCategoryByOrderByNameAsc()){
+		// System.out.println(category);
+		// }
 
-		System.out.println("Affichage de toutes les catégories par ordre décroissant : ");
-		for(Category category : categoryService.getAllCategoryByOrderByNameDesc()){
-			System.out.println(category);
-		}
+		// System.out.println("Affichage de toutes les catégories par ordre décroissant
+		// : ");
+		// for(Category category : categoryService.getAllCategoryByOrderByNameDesc()){
+		// System.out.println(category);
+		// }
 
-		// System.out.println("Suppression des articles moins de 301€");
-		// articleService.deleteArticleByPriceLessThan(301);
+		// // System.out.println("Suppression des articles moins de 301€");
+		// // articleService.deleteArticleByPriceLessThan(301);
 
 	}
 }
-
