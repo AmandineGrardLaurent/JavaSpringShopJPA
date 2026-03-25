11b72a4f43e4e007e515c215c325fae43309b469
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-03-25 10:49:38 +0100

    reformatage de ArticleRepository

--- src/main/java/fr/fms/dao/ArticleRepository.java
+++ src/main/java/fr/fms/dao/ArticleRepository.java
@@ -6,12 +6,16 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.Modifying;
 
 import fr.fms.entities.Article;
+
 import java.util.List;
 
-public interface ArticleRepository extends JpaRepository<Article,Long>{
+public interface ArticleRepository extends JpaRepository<Article, Long> {
     public List<Article> findByBrand(String brand);
+
     public List<Article> findByBrandContains(String brand);
+
     public List<Article> findByBrandAndPrice(String brand, double price);
+
     public List<Article> findByCategoryId(Long categoryId);
 
     public List<Article> findByBrandContainingAndDescriptionContaining(String brand, String description);
