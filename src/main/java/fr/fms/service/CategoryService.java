4e05949749f4b61ef75aeee24e71121cdc6487f8
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-03-25 10:47:49 +0100

    - ajout des méthodes categoryExists et getCategoryById dans le CategoryService

--- src/main/java/fr/fms/service/CategoryService.java
+++ src/main/java/fr/fms/service/CategoryService.java
@@ -2,6 +2,7 @@ package fr.fms.service;
 
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 import fr.fms.dao.CategoryRepository;
 import fr.fms.entities.Category;
@@ -21,4 +22,13 @@ public class CategoryService {
     public List<Category> getAllCategoryByOrderByNameDesc() {
         return categoryRepository.findAllByOrderByNameDesc();
     }
+
+    public Optional<Category> getCategoryById(Long categoryId) {
+        return categoryRepository.findById(categoryId);
+    }
+
+    public boolean categoryExists(Long categoryId) {
+        return categoryRepository.existsById(categoryId);
+    }
+
 }
