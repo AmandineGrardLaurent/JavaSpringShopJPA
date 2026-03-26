db6e6896707ed16bb6d899ff4de81ac97b1408e7
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-03-26 10:39:42 +0100

    - ajout de la méthode displayPagination pour l'affichage de la numérotation des pages - ajout de  la méthode displayArticlesByPage

--- src/main/java/fr/fms/utils/Helper.java
+++ src/main/java/fr/fms/utils/Helper.java
@@ -1,5 +1,7 @@
 package fr.fms.utils;
 
+import org.springframework.data.domain.Page;
+
 import java.util.Collection;
 import java.util.List;
 import java.util.Scanner;
@@ -89,7 +91,7 @@ public class Helper {
      */
     public static int displayChoices(Scanner scanner) {
         System.out.println("[1] Afficher tous les articles sans pagination\n"
-                + "[2] Afficher tous les articles sans pagination\n"
+                + "[2] Afficher tous les articles avec pagination\n"
                 + "********************************** \n"
                 + "[3] Ajouter un article\n"
                 + "[4] Afficher un article\n"
@@ -125,6 +127,97 @@ public class Helper {
             }
             System.out.println("\n");
         }
+    }
+
+    /**
+     * Affiche la pagination des pages disponibles en mettant en évidence
+     * la page courante entre crochets.
+     *
+     * Exemple d'affichage :
+     * Pages : 1 2 [3] 4 5
+     *
+     * @param page objet Page contenant les informations de pagination
+     */
+    public static void displayPagination(Page<?> page) {
+        int totalPages = page.getTotalPages();
+        int currentPage = page.getNumber();
+
+        System.out.print(ConsoleColors.GREEN + "Pages : ");
+
+        for (int i = 0; i < totalPages; i++) {
+            if (i == currentPage) {
+                System.out.print("[" + (i + 1) + "] ");
+            } else {
+                System.out.print((i + 1) + " ");
+            }
+        }
+
+        System.out.println("\n" + ConsoleColors.RESET);
+    }
+
+    /**
+     * Permet d'afficher les articles avec un système de pagination interactif.
+     * L'utilisateur peut naviguer entre les pages, changer le nombre d'éléments
+     * par page ou quitter l'affichage.
+     *
+     * Fonctionnalités :
+     * - Navigation page précédente / suivante
+     * - Modification du nombre d'articles par page
+     * - Affichage de la pagination
+     */
+    public static void displayArticlesByPage(Scanner scanner, ArticleService articleService) {
+        int page = 0;
+        int size = 5;
+        boolean exit = false;
+        Page<Article> articlePage;
+
+        while (!exit) {
+            articlePage = articleService.getArticlesByPage(page, size);
+            System.out.printf(ConsoleColors.GREEN + "\n Page n°%d \n" + ConsoleColors.RESET, (page + 1));
+
+            for (Article article : articlePage.getContent()) {
+                System.out.println(article);
+            }
+
+            displayPagination(articlePage);
+
+            System.out
+                    .println(ConsoleColors.BLUE + "\n [1] page précédent"
+                            + "\n [2] page suivante"
+                            + "\n [3] pagination"
+                            + "\n [4] quitter \n" + ConsoleColors.RESET);
+            int choice = askInt(scanner, "Faîtes votre choix : ");
+
+            switch (choice) {
+                case 1:
+                    if (page > 0) {
+                        page--;
+                    } else {
+                        System.out
+                                .println(ConsoleColors.RED + "Vous êtes déjà à la première page" + ConsoleColors.RESET);
+                    }
+                    break;
+                case 2:
+                    if (page < articlePage.getTotalPages() - 1) {
+                        page++;
+                    } else {
+                        System.out
+                                .println(ConsoleColors.RED + "Vous êtes déjà à la dernière page" + ConsoleColors.RESET);
+                    }
+                    break;
+                case 3:
+                    size = askInt(scanner, "Combien d'articles par page ? ");
+                    page = 0;
+                    break;
+                case 4:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println(ConsoleColors.RED + "Choix invalide" + ConsoleColors.RESET);
+            }
+
+        }
+
     }
 
     /**
