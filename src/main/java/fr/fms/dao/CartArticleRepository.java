021276d0bd21542571b3db2e7c49f3e41d2ba88d
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-04-01 14:01:53 +0200

    ajout de CartArticleRepository - ajout de la méthode findByCartId

---
+++ src/main/java/fr/fms/dao/CartArticleRepository.java
@@ -0,0 +1,13 @@
+package fr.fms.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.fms.entities.Cart;
+import fr.fms.entities.CartArticle;
+
+public interface CartArticleRepository extends JpaRepository<CartArticle, Long> {
+    public List<CartArticle> findByCartId(Long cartId);
+}
