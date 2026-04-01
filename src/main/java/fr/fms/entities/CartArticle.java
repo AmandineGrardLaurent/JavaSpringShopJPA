6f2a8a2961be25061d4aeff804b4eff854b3df6f
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-04-01 14:00:48 +0200

    ajout de l'entité CartArticle

---
+++ src/main/java/fr/fms/entities/CartArticle.java
@@ -0,0 +1,31 @@
+package fr.fms.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+
+@Data
+@Entity
+public class CartArticle implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    private Article article;
+
+    private int quantity;
+}
