93fa5db2a763c08bb3dd3f02dca2f1746f1e549b
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-04-02 09:48:25 +0200

    ajout des méthodes save et edit dans CartController

--- src/main/java/fr/fms/web/CartController.java
+++ src/main/java/fr/fms/web/CartController.java
@@ -1,19 +1,23 @@
 package fr.fms.controller;
 
+import fr.fms.dao.ArticleRepository;
 import fr.fms.dao.CartArticleRepository;
 import fr.fms.dao.CartRepository;
 import fr.fms.dao.UserRepository;
 
 import fr.fms.entities.CartArticle;
 import fr.fms.entities.User;
+import fr.fms.entities.Article;
 import fr.fms.entities.Cart;
 import org.springframework.ui.Model;
 
+import java.util.Optional;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CartController {
@@ -26,15 +30,17 @@ public class CartController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    ArticleRepository articleRepository;
+
     @GetMapping("/cart")
     public String cart(Model model) {
         // user id hardcoded before implementing authentification
-        Long userId = 2L;
+        Long userId = 1L;
 
         Cart cart = cartRepository.findByUserId(userId)
                 .orElseGet(() -> {
                     Cart newCart = new Cart();
-
                     User user = userRepository.findById(userId).orElse(null);
                     newCart.setUser(user);
 
@@ -44,6 +50,58 @@ public class CartController {
 
         model.addAttribute("cartArticleList", cartArticlesList);
         return "cart";
+    }
+
+    @PostMapping("/cart/save")
+    public String save(Long articleId, int quantity) {
+
+        Long userId = 1L;
+
+        Cart cart = cartRepository.findByUserId(userId)
+                .orElseGet(() -> {
+                    Cart newCart = new Cart();
+                    User user = userRepository.findById(userId).orElse(null);
+                    newCart.setUser(user);
+                    return cartRepository.save(newCart);
+                });
+
+        Article article = articleRepository.findById(articleId)
+                .orElseThrow(() -> new RuntimeException("Article non trouvé"));
+
+        Optional<CartArticle> optionalCartArticle = cartArticleRepository.findByCartIdAndArticleId(cart.getId(),
+                articleId);
+
+        CartArticle cartArticle;
+
+        if (optionalCartArticle.isPresent()) {
+            cartArticle = optionalCartArticle.get();
+            cartArticle.setQuantity(cartArticle.getQuantity() + quantity);
+        } else {
+            cartArticle = new CartArticle();
+            cartArticle.setCart(cart);
+            cartArticle.setArticle(article);
+            cartArticle.setQuantity(quantity);
+        }
+
+        cartArticleRepository.save(cartArticle);
+
+        return "redirect:/cart";
+    }
+
+    @PostMapping("/cart/edit")
+    public String edit(Long cartArticleId, int quantity) {
+
+        CartArticle cartArticle = cartArticleRepository.findById(cartArticleId)
+                .orElseThrow(() -> new RuntimeException("Article du panier non trouvé"));
+
+        if (quantity <= 0) {
+            cartArticleRepository.delete(cartArticle);
+        } else {
+            cartArticle.setQuantity(quantity);
+            cartArticleRepository.save(cartArticle);
+        }
+
+        return "redirect:/cart";
     }
 
 }
